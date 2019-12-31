b14cbd989259ab55fbfff845853e06323ef6528b
srivatsa <srivatsabc@gmail.com>
2019-12-31 14:09:39 +0530

    Commit

--- airport-locator-app-0.0.3/airport-locator-app-0.0.3-jenkinsfile.js
+++ airport-locator-app-0.0.3/airport-locator-app-0.0.3-jenkinsfile.js
@@ -137,12 +137,6 @@ pipeline {
         }
       }
 
-    stage('OpenShift Login') {
-        steps {
-          sh 'oc login -u okd-admin https://192.168.1.50:8443 -p root --insecure-skip-tls-verify'
-        }
-    }
-
     stage('OpenShift deployment') {
       steps {
         sh 'oc whoami'
